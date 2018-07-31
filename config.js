6db52ba92ba50193d9b6f3f0c71c7f3132f645dc
Logan Han <logan.han@rea-group.com>
2018-07-31 13:31:26 +1000

    WIP

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+  proxyhost: "blah",
+};
+
+module.exports = config
--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+var config = require("./config.js");
 var proxy = require('express-http-proxy');
 var app = require('express')();
 var download = require('download-to-file');
+var URL = require('url-parse');
 
-/* 
+/*
 console.log('Downloading to /tmp/example.html')
 download('http://example.com/', '/tmp/example.html', function (err, filepath) {
   if (err) throw err
   console.log('Download finished:', filepath)
 })
 
-app.use('/proxy', proxy('www.google.com', {
-  proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-    // you can update headers
-    proxyReqOpts.headers['Content-Type'] = 'text/html';
-    // you can change the method
-    proxyReqOpts.method = 'GET';
-    return proxyReqOpts;
-  }
-}));
 */
+var CacheCheck = function(req, res, next)
+{
+  var url = new URL(req.url);
+  var filepath = url.pathname.match(/(.*)media_/)[1];
+  var filenumber = url.pathname.match(/media_(.*).ts/)[1];
+  console.log("Pathname:" + url.pathname);
+  console.log("Query:" + url.query);
+  console.log("Filepath:" + filepath);
+  console.log("Filenumber:" + filenumber);
+  // call async download here
+  /*
+  for(var i = filenumber; i < filenumber+20; i++) {
+    // check if the file exist first
+    var downloadpath = filepath + "media_" + i + ".ts";
+    console.log("Downloadpath:" + downloadpath)
+    download('http://' + config.proxyhost + downloadpath, downloadpath, function (err, filepath) {
+      if (err) throw err
+      console.log('Download finished:', filepath)
+    })
+  */
+  // if there's cache file
+  //res.send("test");
+  // if not
+  next();
+}
+
+app.use('/', CacheCheck, proxy(config.proxyhost, {
+  preserveHostHdr: true
+}));
+
+app.listen(3000, () => console.log('listening..'));
